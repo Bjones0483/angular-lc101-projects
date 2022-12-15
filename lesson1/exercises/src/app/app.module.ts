7a717b783e588130d447bb9f4172a683f8bb12f2
Bruce Jones <brucedjonesjr@gmail.com>
2022-12-14 20:09:55 -0600

    app

--- lesson1/exercises/src/app/app.module.ts
+++ lesson1/exercises/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { MovieListComponent } from './movie-list/movie-list.component';
 import { ChoresListComponent } from './chores-list/chores-list.component';
 import { FavPhotosComponent } from './fav-photos/fav-photos.component';
-
+import { PageTitleComponent } from './page-title/page-title.component';
+import { FavLinksComponent } from './fav-links/fav-links.component';
 @NgModule({
   declarations: [
     AppComponent,
